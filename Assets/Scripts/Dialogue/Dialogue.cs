b62bcd7a6bbd4aa2c4ceffe71a18746e35305421
samminmul <74859160+samminmul@users.noreply.github.com>
2026-08-18 03:35:33 +0900

    Revert "비 추가, 대사 내부 구현 작업중"
    This reverts commit 9789753c16db37b9232ec9ba1381971a7396c973.

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -1,41 +1,7 @@
-using NUnit.Framework;
 using UnityEngine;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using System;
 
 public class Dialogue : MonoBehaviour
 {
-    private List<DialogueLineData> dialogueLines;
-    private DialogueLineData curLineData;
-    private EventManager eventManager;
-    
-    private int curLineIndex;
-    private int nextLineIndex;
-    private int weightSum = 0;
-
-    void Awake()
-    {
-        eventManager = FindAnyObjectByType<EventManager>();
-        gotoLine(0);
-    }
-
-    private void gotoLine(int lineIndex)
-    {
-        curLineIndex = lineIndex;
-        curLineData = dialogueLines[lineIndex];
-        nextLineIndex = curLineData.jumpto;
-        weightSum += curLineData.weight;
-
-        if (!string.IsNullOrEmpty(curLineData.trigger)) eventManager.TriggerEvent(curLineData.trigger);
-    }
-
-    public void activateLine(int lineIndex)
-    {
-
-    }
-
-
-
-
+    private string speakerName;
+    private string speakerDescription;
 }
